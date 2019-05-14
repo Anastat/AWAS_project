8abd2dda5b6833a6e298c594ad79548f743d9c67
Anastasiia <30463278+Anastat@users.noreply.github.com>
2019-05-14 20:40:27 +0300

    sequelize

--- server.js
+++ server.js
@@ -3,6 +3,28 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
+const { Pool, Client } = require('pg');
+
+/*const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL
+});*/
+
+//connection to database on Heroku
+
+const client = new Client({
+  user: "hfxtsnyxfoktse",
+  password: "2a348570030a038e7b358a49370d1b7718a093bbbb671539959930dd8e2e10e8",
+  database: "d1im5b5t0mehrn",
+  port: 5432,
+  host: "ec2-46-137-188-105.eu-west-1.compute.amazonaws.com",
+  ssl: true
+});
+
+client.connect();
 
 const app = express();
 
